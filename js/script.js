0d2e55b749138393ab2951617b794c41bf487cb4
Admin <meesalasaipranay4932@gmail.com>
2024-09-16 10:50:24 +0530

    done

---
+++ js/script.js
@@ -0,0 +1,47 @@
+///replace tect in header
+const checkReplace = document.querySelector('.replace-me');
+
+if(checkReplace!==null){
+    const replace = new ReplaceMe(checkReplace,{
+        animation:'animated fadeIn',
+        speed:2000,
+        separator:',',
+        loopCount:'infinite',
+        autoRun:true
+    })
+}
+// Add custom JavaScript here
+function userScroll(){
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll',()=>{
+        if(window.scrollY>50){
+            navbar.classList.add('bg-dark')
+            navbar.classList.add('navbar-sticky')
+        }
+        else{
+            navbar.classList.remove('bg-dark')
+            navbar.classList.remove('navbar-sticky')
+        }
+    });
+}
+document.addEventListener('DOMContentLoaded',userScroll);
+//video modal
+const videoBtn =document.querySelector('.video-btn');
+const videoModal = document.querySelector('#videoModal');
+const video = document.querySelector('#video');
+let videoSrc;
+
+if(videoBtn !== null){
+    videoBtn.addEventListener('click',()=>{
+      videoSrc = videoBtn.getAttribute('data-bs-src')
+    })
+}
+
+if(videoModal!== null){
+    videoModal.addEventListener('shown.bs-modal',()=>{
+     video.setAttribute('src',videoSrc+'?autoplay=1;modestbranding=1;showInfo=0');
+    });
+    videoModal.addEventListener('hide.bs.modal',()=>{
+        video.setAttribute('src',videoSrc);
+    })
+}
